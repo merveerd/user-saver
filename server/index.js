8ba4a52e53755865f7fe74dd7993298a3ce107d8
merve erdogmus <merve.erdogmus@adcolony.com>
2021-04-14 22:23:46 +0300

    add env port

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 //comment out below
