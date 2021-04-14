592833f92a505d6a8cd95e0d17a8a7e78de75d30
merve erdogmus <merve.erdogmus@adcolony.com>
2021-04-14 18:25:55 +0300

    add pm2 and its config file

---
+++ ecosystem.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  apps: [
+    {
+      name: "client",
+      cwd: "/home/ubuntu/usersaver/client",
+      script: "npm",
+      args: "start",
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "server",
+      cwd: "/home/ubuntu/usersaver/client",
+      script: "npm",
+      args: "start",
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+  deploy: {
+    production: {
+      user: "ubuntu",
+      host: "18.192.78.125",
+      path: "/home/ubuntu/usersaver",
+      repo: "https://github.com/merveerd/user-saver.git",
+      ref: "origin/master",
+      key: "/Users/adcolony/Documents/aws/user-recorder.pem",
+      "post-deploy": "npm i; pm2 reload ecosystem.config.js --env production",
+    },
+  },
+};
